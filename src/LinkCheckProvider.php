84cd927a9751a2654955286078b9afcdd7d9ece8
Maik Greubel <greubel@nkey.de>
2017-09-03 11:17:47 +0200

    Add documentation and assertions to unit test

--- src/LinkCheckProvider.php
+++ src/LinkCheckProvider.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * This file is part of Nkey/LinkCheck package
+ * 
+ * @package Nkey\LinkCheck
+ */
 namespace Nkey\LinkCheck;
 
 use Generics\Client\HttpClientFactory;
@@ -6,34 +11,66 @@ use Generics\Socket\Url;
 use Generics\Streams\StandardOutputStream;
 use Generics\Streams\HttpStream;
 use Generics\Util\UrlParser;
+use Generics\Streams\OutputStream;
 
+/**
+ * This class provides the link checking functionality.
+ * 
+ * @author Maik Greubel <greubel@nkey.de>
+ */
 class LinkCheckProvider
 {
+
     /**
-     * 
+     *
      * @var HttpStream
      */
     private $httpClient;
 
     /**
-     * 
-     * @var string
+     *
+     * @var OutputStream
      */
     private $output;
-    
+
     /**
-     * 
+     *
      * @var Url
      */
     private $url;
 
+    /**
+     * Create a new instance of LinkCheckProvider
+     * 
+     * @param Url $url
+     *            The url to check
+     */
     public function __construct(Url $url)
     {
         $this->httpClient = HttpClientFactory::get($url);
         $this->output = new StandardOutputStream();
         $this->url = $url;
     }
+    
+    /**
+     * Override the standard output stream using a custom output stream instance
+     * 
+     * @param OutputStream $stream
+     */
+    public function setOutput(OutputStream $stream)
+    {
+        $this->output = $stream;
+    }
 
+    /**
+     * Perform check
+     * - Retrieve the contents of url
+     * - Extract all anchor links
+     * - Check all anchor links for existance (response code < 400)
+     *
+     * @param array $options
+     *            The options for check ('recursive' => perform a recursive check of all same-site-links)
+     */
     public function check(array $options = array())
     {
         $this->httpClient->request('GET');
@@ -45,7 +82,7 @@ class LinkCheckProvider
         
         $response = "";
         $size = $this->httpClient->getPayload()->count();
-        if(isset($this->httpClient->getHeaders()['Content-Length'])) {
+        if (isset($this->httpClient->getHeaders()['Content-Length'])) {
             $size = $this->httpClient->getHeaders()['Content-Length'];
         }
         
@@ -55,7 +92,7 @@ class LinkCheckProvider
         
         $this->extractAndCheckUrls($response, $options);
     }
-    
+
     private function extractAndCheckUrls(string $response, array $options)
     {
         $matches = [];
@@ -65,35 +102,34 @@ class LinkCheckProvider
             $matches = $matches[0];
         }
         
-        if(count($matches) == 0) {
+        if (count($matches) == 0) {
             return;
         }
         
         foreach ($matches as $match) {
-            if(substr($match, 0, 4) != 'http') {
+            if (substr($match, 0, 4) != 'http') {
                 $match = sprintf("%s://%s:%d%s", $this->url->getScheme(), $this->url->getAddress(), $this->url->getPort(), $match);
             }
             $url = UrlParser::parseUrl($match);
             
             $this->output->write($url);
-            if($url->getAddress() == $this->url->getAddress()) {
+            if ($url->getAddress() == $this->url->getAddress()) {
                 $this->checkSameSiteurl($url, $options);
             }
             $this->output->write("\n");
         }
     }
-    
+
     private function checkSameSiteUrl(Url $url, array $options)
     {
-        if(isset($options['recursive']) && $options['recursive'] == true) {
+        if (isset($options['recursive']) && $options['recursive'] == true) {
             $subProvider = new LinkCheckProvider($url);
             $subProvider->check($options);
-        }
-        else {
+        } else {
             $http = HttpClientFactory::get($url);
             $http->setTimeout(10);
             $http->request('HEAD');
-            if($http->getResponseCode() < 400) {
+            if ($http->getResponseCode() < 400) {
                 $this->output->write(" OK");
             }
         }
--- tests/LinkCheckProviderTest.php
+++ tests/LinkCheckProviderTest.php
@@ -4,6 +4,8 @@ namespace Nkey\LinkCheck\Tests;
 use PHPUnit\Framework\TestCase;
 use Nkey\LinkCheck\LinkCheckProvider;
 use Generics\Util\UrlParser;
+use Generics\Streams\StandardOutputStream;
+use Generics\Streams\Interceptor\CachedStreamInterceptor;
 
 class LinkCheckProviderTest extends TestCase
 {
@@ -14,6 +16,13 @@ class LinkCheckProviderTest extends TestCase
     {
         $provider = new LinkCheckProvider(UrlParser::parseUrl("https://letsencrypt.org/getting-started/"));
         
+        $stream = new StandardOutputStream();
+        $interceptor = new CachedStreamInterceptor();
+        $stream->setInterceptor($interceptor);
+        $provider->setOutput($stream);
+        
         $provider->check();
+        
+        $this->assertContains("https://letsencrypt.org/privacy/ OK", $interceptor->getCache());
     }
 }
